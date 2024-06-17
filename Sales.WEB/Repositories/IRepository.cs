debb220deac1d4d219a8f5a25b72de19b725b5a9
Angel <19030347@itesa.edu.mx>
2024-06-17 01:49:54 -0600

    Versión06. Componentes, crear países, alertas SweetAlert2 Blazor

--- Sales.WEB/Repositories/IRepository.cs
+++ Sales.WEB/Repositories/IRepository.cs
@@ -2,8 +2,23 @@
 {
     public interface IRepository
     {
+        // GET
         Task<HttpResponseWrapper<T>> Get<T>(string url);
+
+        // POST
         Task<HttpResponseWrapper<object>> Post<T>(string url, T model);
+
+        // POST que responde
         Task<HttpResponseWrapper<TResponse>> Post<T, TResponse>(string url, T model);
+
+        // DELETE
+        Task<HttpResponseWrapper<object>> Delete(string url);
+
+        // PUT
+        Task<HttpResponseWrapper<object>> Put<T>(string url, T model);
+
+        // PUT que responde
+        Task<HttpResponseWrapper<TResponse>> Put<T, TResponse>(string url, T model);
+
     }
 }
